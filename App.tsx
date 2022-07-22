fc2569315126f4182d25fa4502bf598ea25c697c
frederico lopes <fredericolopes@fredericos-MacBook-Pro.local>
2022-07-21 22:47:55 -0300

    general improvements

--- App.tsx
+++ App.tsx
@@ -7,9 +7,9 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { SignIn } from "./src/screens/SignIn";
 import { THEME } from "./src/styles/theme";
 import { Loading } from "./src/components/Loading";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -22,7 +22,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded === true ? <SignIn /> : <Loading />}
+      {fontsLoaded === true ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
