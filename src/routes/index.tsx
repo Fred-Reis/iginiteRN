3cd8ed066ba4adac8781a2fef0d41b2c82168492
frederico lopes <fredericolopes@fredericos-MacBook-Pro.local>
2022-07-24 16:11:47 -0300

    routes updated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 
 import { AppRoutes } from "./app.routes";
 import { SignIn } from "../screens/SignIn";
+import { Loading } from "../components/Loading";
 
 export const Routes = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  useEffect(() => {
+    const subscribe = auth().onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return subscribe;
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
-      {/* <SignIn /> */}
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 };
